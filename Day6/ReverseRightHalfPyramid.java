bdbba0b0f5ade425921b9567c873f67d72b62d30
Dinesh A <dineshkavitha2005@gmail.com>
2026-08-25 21:31:05 +0530

    Add files via upload

---
+++ Day6/ReverseRightHalfPyramid.java
@@ -0,0 +1,12 @@
+package pattern;
+public class ReverseRightHalfPyramid {
+    public static void main(String[] args) {
+        int n=5;
+        for (int row = n; row>=1; row--){
+            for(int col =row; col>=1;col--){
+                System.out.print("*"+ " ");
+            }
+System.out.println();
+        }
+    }
+}
